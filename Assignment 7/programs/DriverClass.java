84c62f7d9e08e449925a1425fe9335fe4ff889b2
BIBHUTI BHUSAN MOHANTY <bibhutibhusanmohanty6504@gmail.com>
2023-12-29 14:32:02 +0530

    all A7 update

--- Assignment 7/programs/DriverClass.java
+++ Assignment 7/programs/DriverClass.java
@@ -2,13 +2,10 @@ import mypack.Display;
 
 public class DriverClass {
     public static void main(String[] args) {
-        // Demonstrate different constructors of Display class
         Display display1 = new Display(42);
         Display display2 = new Display(3.14f);
         Display display3 = new Display(new int[] { 1, 2, 3, 4, 5 });
         Display display4 = new Display("Hello, World!");
         Display display5 = new Display(new String[] { "Java", "is", "fun" });
-
-        // You can create and use more Display objects as needed
     }
 }
